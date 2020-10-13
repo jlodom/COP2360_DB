3d5d5e442d1c00f2b50beb1a7fa31c715e613ee5
Johnnie Lamar Odom II <jlodom00@alumni.amherst.edu>
2020-10-13 17:10:48 -0500

    October 6 (Class 1) Commit

---
+++ Cop2360_Databases_Interfaces/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using Microsoft.Data.Sqlite;
+using DBInterfaceExampleLib;
+
+namespace Cop2360_Databases_Interfaces {
+	class Program {
+		static void Main(string[] args) {
+
+
+			/* Create a Factory that can in turn create an object conforming to our interface. */
+			DBInterfaceExampleFactory factory = new DBInterfaceExampleFactory();
+			IDBInterfaceExample dblib = factory.CreateDBInterfaceExample((int)DBInterfaceExampleFactory.DBINTERFACETYPE.sqlite);
+			dblib.SetDBName("example.sqlite");
+			Console.WriteLine(dblib.GetDBName());
+
+
+			/* Create a SQLite connection. Specify a file and create if it does not exist.
+			 * Use a connection string to connect to the database.
+			 * For most SQL servers you will need server name, database name, username, password
+			 * and maybe some additional parameters about the connection.
+			 * For SQLite you just need a path to a SQLite file (which uses .sqlite or .db extensions )
+			 */
+			//SqliteConnection dbconnection = new SqliteConnection("Data Source=example.sqlite;");
+			//dbconnection.Open(); /* Write the close connection when you write the open. */
+			/* Using a StringBuilder is recommended when building SQL strings to make the process a bit safer. */
+			//StringBuilder sbCreateTable = new StringBuilder();
+			//String stringTableName = "CANDY";
+			/* Usually building a SQL string will be more complex depending on variables. */
+			//sbCreateTable.Append("CREATE TABLE " + stringTableName + "('name' varchar, 'color' varchar, 'nuts' varchar)");
+			//Console.WriteLine(sbCreateTable); /* We are just showing what we are going to send. Debug. */
+			/* Now create a SQL command that we will execute. Need both the command string and the connection. */
+			//SqliteCommand scmdCreateTable = new SqliteCommand(sbCreateTable.ToString(), dbconnection);
+			/* This command will be sent as text, and it will not return any data. */
+			//.CommandType = CommandType.Text;
+			//int intCommandSuccessCode = scmdCreateTable.ExecuteNonQuery();
+			//scmdCreateTable.Dispose();
+
+			//dbconnection.Close(); /* Write this when you write the open. */
+
+			/* Create a database on disk (SQLIte does this, other DBs not so much. */
+
+
+
+		}
+	}
+}
---
+++ DBInterfaceExampleLib/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DBInterfaceExampleLib {
+
+
+	/* Interfaces by convention start with "I" followed by the name of the class they go to. */
+	public interface IDBInterfaceExample {
+
+		Boolean CreateATable(String stringTableName, List<String> ColumnNames);
+		Boolean AddRowsToTable(List<OrderedDictionary> loRows);
+		List<Dictionary<String,String>> GetRowsFromTable(String stringTableName);
+		String GetDBName();
+		Boolean SetDBName(String stringNewDBName);
+
+	}
+
+
+
+	public abstract class DBInterfaceExample : IDBInterfaceExample {
+
+		/* Variables belong to class */
+		String dbname = "NODB";
+
+
+		/* Methods required by the interface */
+		public abstract Boolean CreateATable(String stringTableName, List<String> ColumnNames);
+		public abstract Boolean AddRowsToTable(List<OrderedDictionary> loRows);
+		public abstract List<Dictionary<String, String>> GetRowsFromTable(String stringTableName);
+		public String GetDBName() {
+			return this.dbname;
+
+		}
+		public Boolean SetDBName(String stringNewDBName) {
+			Boolean boolReturn = true;
+			this.dbname = stringNewDBName;
+			return boolReturn;
+		}
+
+	}
+
+
+
+	public class SqliteInterfaceExample : DBInterfaceExample, IDBInterfaceExample {
+
+		public override Boolean CreateATable(String stringTableName, List<String> ColumnNames) {
+
+			return true;
+		}
+
+		public override Boolean AddRowsToTable(List<OrderedDictionary> loRows) {
+			return true;
+		}
+
+		public override List<Dictionary<String, String>> GetRowsFromTable(String stringTableName) {
+
+			throw new NotImplementedException();
+		}
+
+	}
+
+
+	public class DBInterfaceExampleFactory {
+
+		public enum DBINTERFACETYPE { mssql = 1, postgres, mysql, sqlite };
+
+		public DBInterfaceExampleFactory() {
+
+		}
+
+		public IDBInterfaceExample CreateDBInterfaceExample(int intTypeOfDB) {
+
+			IDBInterfaceExample IDBIEReturn = null;
+
+			switch(intTypeOfDB) {
+				case (int)DBINTERFACETYPE.sqlite:
+					IDBIEReturn = new SqliteInterfaceExample();
+					break;
+			}
+
+			return IDBIEReturn;
+
+		}
+
+	}
+
+
+}
