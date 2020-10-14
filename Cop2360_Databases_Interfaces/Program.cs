cf201218634744ae7c5d2673e6a96a405a3171aa
Johnnie Lamar Odom II <jlodom00@alumni.amherst.edu>
2020-10-13 19:37:07 -0500

    * Program.cs: Added much more sample code midway through class.
    * Class1.cs: Added Connect and Disconnect and fleshed out create
      table.

--- Cop2360_Databases_Interfaces/Program.cs
+++ Cop2360_Databases_Interfaces/Program.cs
@@ -3,45 +3,72 @@ using System.Text;
 using System.Data;
 using Microsoft.Data.Sqlite;
 using DBInterfaceExampleLib;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
 
 namespace Cop2360_Databases_Interfaces {
 	class Program {
 		static void Main(string[] args) {
 
+			/* Arguments */
+			String stringPathToInputFile = args[0];
+			String stringDatabasePathOrDatabaseServer = args[1];
+
 
 			/* Create a Factory that can in turn create an object conforming to our interface. */
 			DBInterfaceExampleFactory factory = new DBInterfaceExampleFactory();
 			IDBInterfaceExample dblib = factory.CreateDBInterfaceExample((int)DBInterfaceExampleFactory.DBINTERFACETYPE.sqlite);
-			dblib.SetDBName("example.sqlite");
+			dblib.SetDBName(stringDatabasePathOrDatabaseServer);
 			Console.WriteLine(dblib.GetDBName());
 
 
-			/* Create a SQLite connection. Specify a file and create if it does not exist.
-			 * Use a connection string to connect to the database.
-			 * For most SQL servers you will need server name, database name, username, password
-			 * and maybe some additional parameters about the connection.
-			 * For SQLite you just need a path to a SQLite file (which uses .sqlite or .db extensions )
-			 */
-			//SqliteConnection dbconnection = new SqliteConnection("Data Source=example.sqlite;");
-			//dbconnection.Open(); /* Write the close connection when you write the open. */
-			/* Using a StringBuilder is recommended when building SQL strings to make the process a bit safer. */
-			//StringBuilder sbCreateTable = new StringBuilder();
-			//String stringTableName = "CANDY";
-			/* Usually building a SQL string will be more complex depending on variables. */
-			//sbCreateTable.Append("CREATE TABLE " + stringTableName + "('name' varchar, 'color' varchar, 'nuts' varchar)");
-			//Console.WriteLine(sbCreateTable); /* We are just showing what we are going to send. Debug. */
-			/* Now create a SQL command that we will execute. Need both the command string and the connection. */
-			//SqliteCommand scmdCreateTable = new SqliteCommand(sbCreateTable.ToString(), dbconnection);
-			/* This command will be sent as text, and it will not return any data. */
-			//.CommandType = CommandType.Text;
-			//int intCommandSuccessCode = scmdCreateTable.ExecuteNonQuery();
-			//scmdCreateTable.Dispose();
-
-			//dbconnection.Close(); /* Write this when you write the open. */
-
-			/* Create a database on disk (SQLIte does this, other DBs not so much. */
-
-
+			/* Code Adapted from Previous Notes to Give Us Some Data To Work With */
+			/* (We do not need all of this.) */
+			Console.Write("\n\n\nPROGRAM STARTS HERE\n");
+			Decimal decMaxContribution = 500.00m;
+			Decimal decSmallContributionThreshold = 100m;
+			/* Make sure the file exists before proceeding. */
+			if(File.Exists(stringPathToInputFile)) {
+				String stringEntireText = File.ReadAllText(stringPathToInputFile); /* Read the whole file into memory as a string (could go line by line. */
+				String stringNewLine = "\r\n"; /* Force using Windows newline instead of Environment.NewLine */
+				Char charDelimiter = '\t'; /* This file uses tabs, but we could change this variable if other files use other delimiters */
+				String[] arrayStringLines = stringEntireText.Split(stringNewLine); /* Convert the big string into an array of rows */
+				/* Create some more variables we will need. */
+				int lineNumber = 0;
+				List<String> listStringHeader = new List<String>();
+				List<OrderedDictionary> listOrdicRows = new List<OrderedDictionary>();
+				/* Go through every line/row and process appropriately. */
+				foreach(String stringLine in arrayStringLines) {
+					lineNumber++;
+					/* If a line is blank, do not do anything with it. */
+					if(String.IsNullOrWhiteSpace(stringLine)) {
+						Console.WriteLine("Blank Line");
+					}
+					else {
+						/* Create a list to holding header information if this is the first line (the header). */
+						if(lineNumber == 1) {
+							String[] arrayStringTempHeader = stringLine.Split(charDelimiter);
+							foreach(String stringHeaderColumn in arrayStringTempHeader) {
+								listStringHeader.Add(stringHeaderColumn.Replace(' ', '_').Replace('/', '_').Trim().ToLower());
+								/* We can dice the header string a lot in just one line (above). */
+							}
+						}
+						/* If this is not the first line, treat it like a data row. */
+						else {
+							String[] arrayStringTempROw = stringLine.Split(charDelimiter);
+							OrderedDictionary ordicRow = new OrderedDictionary();
+							for(int i = 0; i < listStringHeader.Count; i++) {
+								ordicRow.Add(listStringHeader[i], arrayStringTempROw[i]);
+								/* (Above) Add each column in the row to an ordered dictionary keyed to its header name. 
+								 We can look up this column data in the future either by its order by its header name. */
+							}
+							listOrdicRows.Add(ordicRow);
+						}
+					}
+				}
+			}
+			/* New code goes here again. */
 
 		}
 	}
--- DBInterfaceExampleLib/Class1.cs
+++ DBInterfaceExampleLib/Class1.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Linq;
+using System.Data;
+using Microsoft.Data.Sqlite;
 
 namespace DBInterfaceExampleLib {
 
@@ -14,6 +17,8 @@ namespace DBInterfaceExampleLib {
 		List<Dictionary<String,String>> GetRowsFromTable(String stringTableName);
 		String GetDBName();
 		Boolean SetDBName(String stringNewDBName);
+		Boolean ConnectToDB();
+		Boolean DisconnectFromDB();
 
 	}
 
@@ -29,6 +34,8 @@ namespace DBInterfaceExampleLib {
 		public abstract Boolean CreateATable(String stringTableName, List<String> ColumnNames);
 		public abstract Boolean AddRowsToTable(List<OrderedDictionary> loRows);
 		public abstract List<Dictionary<String, String>> GetRowsFromTable(String stringTableName);
+		public abstract Boolean ConnectToDB();
+		public abstract Boolean DisconnectFromDB();
 		public String GetDBName() {
 			return this.dbname;
 
@@ -45,9 +52,89 @@ namespace DBInterfaceExampleLib {
 
 	public class SqliteInterfaceExample : DBInterfaceExample, IDBInterfaceExample {
 
+		String stringDBFilePath = String.Empty;
+		SqliteConnection dbconnection;
+
+		/* This is NOT recommended, Just here to speed things along in class. */
+		public Boolean SetConnectionVariables(String stringTempDBFilePath) {
+			stringDBFilePath = stringTempDBFilePath;
+			return true;
+		}
+
+		public override Boolean ConnectToDB() {
+			Boolean boolConnectionSucceeded = false;
+			try {
+				dbconnection = new SqliteConnection("Data Source=" + stringDBFilePath + ";");
+				boolConnectionSucceeded = true;
+			}
+			catch {
+				boolConnectionSucceeded = false;
+			}
+			return boolConnectionSucceeded;
+		}
+
+		public override bool DisconnectFromDB() {
+			Boolean boolDisconnectSucceeded = true;
+			try {
+				dbconnection.Close();
+			}
+			catch {
+				boolDisconnectSucceeded = false;
+			}
+			return boolDisconnectSucceeded;
+		}
+
 		public override Boolean CreateATable(String stringTableName, List<String> ColumnNames) {
 
-			return true;
+			/* Create the column create SQL statement while checking that the table and column names are valid. */
+			Boolean boolProceed = false;
+			boolProceed = IsAlphaNumeric(stringTableName);
+			if(boolProceed) {
+				foreach(String stringColumnName in ColumnNames) {
+					if(!(IsAlphaNumeric(stringColumnName))) {
+						boolProceed = false;
+					}
+				}
+			}
+			if(boolProceed) {
+				StringBuilder sbCreateTable = new StringBuilder();
+				sbCreateTable.Append("CREATE TABLE tablename (");
+				Boolean boolIsNotFirst = false;
+				foreach(String stringColumnName in ColumnNames) {
+					if(boolIsNotFirst) {
+						sbCreateTable.Append(",");
+					}
+					else {
+						boolIsNotFirst = true;
+					}
+					sbCreateTable.Append(" " + stringColumnName + " varchar");
+				}
+				sbCreateTable.Append(");");
+				String stringColumnCreate = sbCreateTable.ToString();
+				Console.WriteLine(stringColumnCreate); // DELETE ME
+
+				/* Create the table. */
+				try {
+					this.ConnectToDB();
+					SqliteCommand scmdCreateTable = new SqliteCommand(stringColumnCreate, dbconnection);
+					/* This command will be sent as text, and it will not return any data. */
+					scmdCreateTable.CommandType = CommandType.Text;
+					int intCommandSuccessCode = scmdCreateTable.ExecuteNonQuery();
+					scmdCreateTable.Dispose();
+					this.DisconnectFromDB();
+				}
+				catch {
+					boolProceed = false;
+				}
+				/* Standard SQL Create Table Command
+				 * CREATE TABLE tablename (column1 datatype, column2 datatype, column3 datatype);
+				 * etc. etc. */
+			}
+			else {
+				boolProceed = false;
+			}
+
+			return boolProceed;
 		}
 
 		public override Boolean AddRowsToTable(List<OrderedDictionary> loRows) {
@@ -57,6 +144,23 @@ namespace DBInterfaceExampleLib {
 		public override List<Dictionary<String, String>> GetRowsFromTable(String stringTableName) {
 
 			throw new NotImplementedException();
+		}
+
+
+
+		/* https://stackoverflow.com/questions/1046740/how-can-i-validate-a-string-to-only-allow-alphanumeric-characters-in-it */
+		static Boolean IsAlphaNumeric(String stringPossibleAlphaNumeric) {
+			Boolean boolValid = false;
+			/* Probably unnecessary, but paranoia has set in. */
+			if(String.IsNullOrWhiteSpace(stringPossibleAlphaNumeric)) {
+				boolValid = false;
+			}
+			else {
+				if(stringPossibleAlphaNumeric.All(char.IsLetterOrDigit)) {
+					boolValid = true;
+				}
+			}
+			return boolValid;
 		}
 
 	}
